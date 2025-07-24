d4f99e59a2b8d917a796e1b7eec10eb595f38f95
masdefry <muhammaddefryan@gmail.com>
2025-07-24 22:07:30 +0700

    day13 done

--- hr-api/src/controllers/timeoff.controller.ts
+++ hr-api/src/controllers/timeoff.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
-export const createTimeOffController = async (
-  req: Request,
-  res: Response
-) => {};
+export const createTimeOffController = async (req: Request, res: Response) => {
+  console.log('Controller');
+  console.log(req.files);
+  console.log(req.body);
+};
