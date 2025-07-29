aee24203f06bcafa97cc0da5fe52f183d5e2d94b
masdefry <muhammaddefryan@gmail.com>
2025-07-29 22:11:19 +0700

    handle upload file on hr-app

--- hr-app/src/app/time-off/request/page.tsx
+++ hr-app/src/app/time-off/request/page.tsx
@@ -1,13 +1,40 @@
 'use client';
 import { MdOutlineDriveFolderUpload } from 'react-icons/md';
 import { PiClockCountdownDuotone } from 'react-icons/pi';
-import { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
 import { IoCalendarOutline } from 'react-icons/io5';
 import { PiListPlusDuotone } from 'react-icons/pi';
 import HeaderTitle from '@/components/HeaderTitle';
+import { useFormik } from 'formik';
+import { requestTimeOffSchema } from '@/features/time-off/request/schemas/requestTimeOffSchema';
+import axios from 'axios';
+
 export default function Page() {
-  const [date, setDate] = useState<Date | undefined>();
+  const onHandleRequestTimeOff = async ({
+    timeOffType,
+    reason,
+    evidence,
+  }: any) => {
+    const formData = new FormData();
+    formData.append('timeOffType', timeOffType);
+    formData.append('reason', reason);
+    evidence.forEach((evidenceItem: any) => {
+      formData.append('evidence', evidenceItem);
+    });
+
+    await axios.post('', formData);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      timeOffType: '',
+      reason: '',
+      date: '',
+      evidence: [] as File[],
+    },
+    validationSchema: requestTimeOffSchema,
+    onSubmit: (values) => {},
+  });
 
   return (
     <>
@@ -18,10 +45,15 @@ export default function Page() {
         <fieldset className='flex items-center gap-2 py-2 border-b-1 border-gray-300'>
           <PiClockCountdownDuotone className='text-2xl text-gray-500' />
           <select
-            defaultValue='Pick a color'
+            id='timeOffType'
+            name='timeOffType'
+            onChange={formik?.handleChange}
+            value={formik?.values?.timeOffType}
             className='select w-full bg-gray-100 text-gray-500 border-none'
           >
             <option>Time off type</option>
+            <option>IZIN_MENIKAH</option>
+            <option>IZIN_SAKIT</option>
           </select>
         </fieldset>
         <fieldset className='flex items-center gap-2 py-2 border-b-1 border-gray-300'>
@@ -31,7 +63,7 @@ export default function Page() {
             className='input bg-gray-100 w-full border-none text-gray-500'
             style={{ anchorName: '--rdp' } as React.CSSProperties}
           >
-            {date ? date.toLocaleDateString() : 'Select date'}
+            {formik?.values?.date ? formik?.values?.date : 'Select date'}
           </button>
           <div
             popover='auto'
@@ -40,25 +72,47 @@ export default function Page() {
             style={{ positionAnchor: '--rdp' } as React.CSSProperties}
           >
             <DayPicker
+              id='date'
               className='react-day-picker'
               mode='single'
-              selected={date}
-              onSelect={setDate}
+              selected={
+                formik.values.date ? new Date(formik.values.date) : undefined
+              }
+              onSelect={(date) => {
+                if (date) {
+                  formik?.setFieldValue('date', date.toISOString());
+                } else {
+                  formik?.setFieldValue('date', '');
+                }
+              }}
             />
           </div>
         </fieldset>
         <fieldset className='flex items-center gap-2 py-2 border-b-1 border-gray-300'>
           <PiListPlusDuotone className='text-2xl text-gray-500' />
           <input
+            id='reason'
+            name='reason'
+            onChange={formik?.handleChange}
+            value={formik?.values?.reason}
             type='text'
             placeholder='Reason'
             className='input border-none text-gray-500 bg-gray-100'
           />
         </fieldset>
         <fieldset className='flex items-center gap-2 py-2 border-b-1 border-gray-300 mt-1'>
-          <legend className='font-bold text-xs text-gray-500'>Upload File Evidence</legend>
+          <legend className='font-bold text-xs text-gray-500'>
+            Upload File Evidence
+          </legend>
           <MdOutlineDriveFolderUpload className='text-2xl text-gray-500' />
           <input
+            id='evidence'
+            name='evidence'
+            onChange={(event) => {
+              const files = Array.from(event.currentTarget.files || []);
+              formik.setFieldValue('evidence', files);
+            }}
+            multiple
             type='file'
             className='file-input w-full'
           />
---
+++ hr-app/src/features/time-off/request/schemas/requestTimeOffSchema.ts
@@ -0,0 +1,17 @@
+import * as Yup from 'yup';
+
+export const requestTimeOffSchema = Yup.object().shape({
+  timeOffType: Yup.string().required('Time off type is required'),
+  reason: Yup.string().required('Reason is required'),
+  evidence: Yup.array().of(
+    Yup.mixed<File>()
+      .test('fileSize', 'Maximum file size is 2 mb', (file) => {
+        return file && file.size <= 2000000;
+      })
+      .test('fileFormat', 'Format file not accepted', (file) => {
+        const fileFormatAccepted = ['jpg', 'jpeg', 'png', 'webp', 'svg'];
+
+        return file && fileFormatAccepted.includes(file?.type?.split('/')[1]);
+      })
+  ),
+});
